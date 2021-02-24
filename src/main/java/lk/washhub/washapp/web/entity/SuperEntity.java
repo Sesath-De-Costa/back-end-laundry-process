3683af40e39ec4f6bc23e938162a9b17a76ad62e
sesath <sesathdecosta@gmail.com>
2021-02-24 22:58:13 +0530

    Add SuperEntity.java

---
+++ src/main/java/lk/washhub/washapp/web/entity/SuperEntity.java
@@ -0,0 +1,12 @@
+package lk.washhub.washapp.web.entity;
+
+import java.io.Serializable;
+
+/**
+ * Created by Sesath De Costa on 2021-02-24
+ * <p>
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+ **/
+public interface SuperEntity extends Serializable {
+}
