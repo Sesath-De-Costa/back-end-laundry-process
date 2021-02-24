9e88cfccb1a5d87dfecf12d57dc00a4f2a49ff37
sesath <sesathdecosta@gmail.com>
2021-02-24 23:14:46 +0530

    Add User entity

---
+++ src/main/java/lk/washhub/washapp/web/entity/User.java
@@ -0,0 +1,33 @@
+package lk.washhub.washapp.web.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+/**
+ * Created by Sesath De Costa on 2021-02-20
+ * <p>
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+ **/
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "user")
+public class User implements SuperEntity {
+    @GeneratedValue
+    @Id
+    private int id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    private String nic;
+    @Embedded
+    private UserType userType;
+    private String mobile;
+
+}
