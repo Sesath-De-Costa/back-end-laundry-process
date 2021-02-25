41631f4c1624dab52702c8f160f9e896cee3f122
sesath <sesathdecosta@gmail.com>
2021-02-25 12:15:59 +0530

    Add LaundryTypes enum

---
+++ src/main/java/lk/washhub/washapp/web/entity/LaundryTypes.java
@@ -0,0 +1,14 @@
+package lk.washhub.washapp.web.entity;
+
+import javax.persistence.Embeddable;
+
+/**
+ * Created by Sesath De Costa on 2021-02-24
+ * <p>
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+ **/
+@Embeddable
+public enum LaundryTypes {
+    GARMENT, BEDSHEET, CURTAINS
+}
