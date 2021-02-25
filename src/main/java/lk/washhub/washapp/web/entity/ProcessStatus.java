c148dbc4f248af94c098043589de5bba91a9ed3c
sesath <sesathdecosta@gmail.com>
2021-02-25 12:14:29 +0530

    Add Stage entity and ProcessStatus enum

---
+++ src/main/java/lk/washhub/washapp/web/entity/ProcessStatus.java
@@ -0,0 +1,9 @@
+package lk.washhub.washapp.web.entity; /**
+   * Created by Sesath De Costa on 2021-02-25
+   *
+   * This file is subject to the terms and conditions defined in
+   * file 'LICENSE.txt', which is part of this source code package.
+   *
+ **/ 
+ public enum ProcessStatus {
+}
--- src/main/java/lk/washhub/washapp/web/entity/Stage.java
+++ src/main/java/lk/washhub/washapp/web/entity/Stage.java
@@ -1,11 +1,10 @@
 package lk.washhub.washapp.web.entity;
 
 /**
- * Created by Sesath De Costa on 2021-02-24
+ * Created by Sesath De Costa on 2021-02-25
  * <p>
  * This file is subject to the terms and conditions defined in
  * file 'LICENSE.txt', which is part of this source code package.
  **/
-public enum ProcessStages {
-    REQUESTED, CANCELED, PICKED, RECEIVED, WASHED, DRIED, IRONED, READY, ON_THE_WAY, COMPLETED
+public class Stage {
 }
