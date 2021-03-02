6cd1d96153209696df3d65914371c3be92a7c05d
sesath <sesathdecosta@gmail.com>
2021-03-02 12:36:49 +0530

    Add StageBO

---
+++ src/main/java/lk/washhub/washapp/web/business/custom/StageBO.java
@@ -0,0 +1,25 @@
+package lk.washhub.washapp.web.business.custom;
+
+import lk.washhub.washapp.web.business.SuperBO;
+import lk.washhub.washapp.web.dto.StageDTO;
+
+import java.util.List;
+
+/**
+ * Created by Sesath De Costa on 2021-03-02
+ * <p>
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+ **/
+public interface StageBO extends SuperBO {
+
+    void saveStage(StageDTO stageDTO) throws Exception;
+
+    void updateStage(StageDTO stageDTO) throws Exception;
+
+    void deleteStage(StageDTO stageDTO) throws Exception;
+
+    StageDTO getStage(StageDTO stageDTO) throws Exception;
+
+    List<StageDTO> getAllStage() throws Exception;
+}
