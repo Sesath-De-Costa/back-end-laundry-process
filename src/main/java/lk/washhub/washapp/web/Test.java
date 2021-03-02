1194335c34695da58412e5f96e17881422a28556
sesath <sesathdecosta@gmail.com>
2021-03-02 12:30:22 +0530

    Add ProcessBO

---
+++ src/main/java/lk/washhub/washapp/web/Test.java
@@ -0,0 +1,13 @@
+package lk.washhub.washapp.web;
+
+/**
+ * Created by Sesath De Costa on 2021-03-02
+ * <p>
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+ **/
+public class Test {
+    public static void main(String[] args) {
+        
+    }
+}
--- src/main/java/lk/washhub/washapp/web/util/LogConfig.java
+++ src/main/java/lk/washhub/washapp/web/util/LogConfig.java
@@ -1,9 +1,9 @@
-package lk.washhub.washapp.web.dao; /**
-   * Created by Sesath De Costa on 2021-03-01
+package lk.washhub.washapp.web.util; /**
+   * Created by Sesath De Costa on 2021-03-02
    *
    * This file is subject to the terms and conditions defined in
    * file 'LICENSE.txt', which is part of this source code package.
    *
  **/ 
- public interface CrudDAO {
+ public class LogConfig {
 }
