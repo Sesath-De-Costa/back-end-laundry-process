55e22d516f1f11b3ac9d0d437d060be5acfbe64c
sesath <sesathdecosta@gmail.com>
2021-03-02 12:33:51 +0530

    Add RequestBO

---
+++ src/main/java/lk/washhub/washapp/web/business/custom/RequestBO.java
@@ -0,0 +1,9 @@
+package lk.washhub.washapp.web.business.custom; /**
+   * Created by Sesath De Costa on 2021-03-02
+   *
+   * This file is subject to the terms and conditions defined in
+   * file 'LICENSE.txt', which is part of this source code package.
+   *
+ **/ 
+ public interface RequestBO {
+}
