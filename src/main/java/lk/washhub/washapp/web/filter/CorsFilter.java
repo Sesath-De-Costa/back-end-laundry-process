43601014b6f0c6156cd99a32ec8564dc2af8f243
sesath <sesathdecosta@gmail.com>
2021-03-08 16:23:11 +0530

    Add Cors filter

--- src/main/java/lk/washhub/washapp/web/filter/CorsFilter.java
+++ src/main/java/lk/washhub/washapp/web/filter/CorsFilter.java
@@ -1,9 +1,31 @@
-package lk.washhub.washapp.web.filter; /**
-   * Created by Sesath De Costa on 2021-03-08
-   *
-   * This file is subject to the terms and conditions defined in
-   * file 'LICENSE.txt', which is part of this source code package.
-   *
- **/ 
- public class CorsFilter {
+package lk.washhub.washapp.web.filter;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Sesath De Costa on 2021-03-08
+ * <p>
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+ **/
+@Component
+@Order(1)
+@WebFilter(urlPatterns = "/*")
+public class CorsFilter extends HttpFilter {
+    @Override
+    protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+        response.setHeader("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        chain.doFilter(request, response);
+    }
 }
